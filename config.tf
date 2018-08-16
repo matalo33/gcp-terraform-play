89994551b7f9c39a2a0fdb45e16d463d853fcd3c
Matthew Taylor <matthew@m-taylor.co.uk>
2018-08-16 16:55:53 -0400

    Remote gcs backend

--- config.tf
+++ config.tf
@@ -1,5 +1,13 @@
 // Google Cloud provider
 provider "google" {
   project     = "cr-lab-mtaylor-1508183413"
-  region      = "us-central1"
+  region      = "${var.gcp_region}"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "matalo-wordpress-tfstate"
+    prefix = "workshop/terraform.tfstate"
+    region = "us-central1"
+  }
 }
